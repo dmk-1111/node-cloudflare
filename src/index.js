73a8a4c8bc2f3381d6dcb97c9d4001f4e7af0cef
mr-dark <motedee02@gmail.com>
2026-03-23 23:01:51 +0700

    up

--- src/index.js
+++ src/index.js
@@ -16,18 +16,88 @@ app.get("/setup", async (c) => {
   try {
     const db = getDB(c.env)
 
+    // menus
     await db.run(sql`
-        CREATE TABLE IF NOT EXISTS employees (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          name TEXT NOT NULL
-        )
-      `)
+      CREATE TABLE IF NOT EXISTS menus(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        status INTEGER DEFAULT 1
+      )
+    `)
 
-    return c.text("Table created!")
+    // greetings
+    await db.run(sql`
+      CREATE TABLE IF NOT EXISTS greetings(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        button_one TEXT,
+        button_two TEXT
+      )
+    `)
+
+    // medias
+    await db.run(sql`
+      CREATE TABLE IF NOT EXISTS medias(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        url TEXT,
+        icon TEXT,
+        status INTEGER DEFAULT 1
+      )
+    `)
+
+    // about
+    await db.run(sql`
+      CREATE TABLE IF NOT EXISTS about(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        content TEXT
+      )
+    `)
+
+    // experience
+    await db.run(sql`
+      CREATE TABLE IF NOT EXISTS experience(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        main_title TEXT,
+        sub_title TEXT,
+        status INTEGER DEFAULT 1
+      )
+    `)
+
+    // skills
+    await db.run(sql`
+      CREATE TABLE IF NOT EXISTS skills(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        status INTEGER DEFAULT 1
+      )
+    `)
+
+    // achieves
+    await db.run(sql`
+      CREATE TABLE IF NOT EXISTS achieves(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        image TEXT,
+        status INTEGER DEFAULT 1
+      )
+    `)
+
+    // footers
+    await db.run(sql`
+      CREATE TABLE IF NOT EXISTS footers(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        copy_right TEXT,
+        title TEXT
+      )
+    `)
+
+    return c.text("All tables created successfully!")
   } catch (err) {
     return c.text(err.message, 500)
   }
-})
+});
 
 app.get("/show-employees", async (c) => {
   try {
--- src/schema.js
+++ src/schema.js
@@ -1,6 +1,54 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core"
 // DEFINE TABLE
-export const employees = sqliteTable("employees", {
+export const menus = sqliteTable("menus", {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  name: text("name").notNull()
-})
+  name: text("name").notNull(),
+  status: integer("status").default(1)
+});
+
+export const greetings = sqliteTable("greetings", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  title: text("title"),
+  button_one: text("button_one"),
+  button_two: text("button_two")
+});
+
+export const medias = sqliteTable("medias", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name"),
+  url: text("url"),
+  icon: text("icon"),
+  status: integer("status").default(1)
+});
+
+export const about = sqliteTable("about", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  title: text("title"),
+  content: text("content")
+});
+
+export const experience = sqliteTable("experience", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  main_title: text("main_title"),
+  sub_title: text("sub_title"),
+  status: integer("status").default(1)
+});
+
+export const skills = sqliteTable("skills", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name"),
+  status: integer("status").default(1)
+});
+
+export const achieves = sqliteTable("achieves", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name"),
+  image: text("image"),
+  status: integer("status").default(1)
+});
+
+export const footers = sqliteTable("footers", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  copy_right: text("copy_right"),
+  title: text("title")
+});
